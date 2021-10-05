14cc67a7b0a538f2947c53dbefd4a87045c664eb
Cotelea Marius <marius.cotelea1003@gmail.com>
2021-10-05 16:57:54 +0300

    I added Firebase database to the project and Sign Up data storage

--- app/src/main/java/com/example/expensemanager/MainActivity.java
+++ app/src/main/java/com/example/expensemanager/MainActivity.java
@@ -2,13 +2,61 @@ package com.example.expensemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mEmail, mPass;
+    private Button btnLogin;
+    private TextView mForgotPassword, mSignUpHere;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginDetails();
+    }
+
+    private void loginDetails() {
+        mEmail = findViewById(R.id.Email_login);
+        mPass = findViewById(R.id.Password_login);
+        btnLogin = findViewById(R.id.btn_login);
+        mForgotPassword = findViewById(R.id.forgot_password);
+        mSignUpHere = findViewById(R.id.signup);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = mEmail.getText().toString().trim();
+                String pass = mPass.getText().toString();
+                if (TextUtils.isEmpty(email)) {
+                    mEmail.setError("Email field is empty.");
+                    return;
+                }
+                if (TextUtils.isEmpty(pass)) {
+                    mPass.setError("Password field is empty.");
+                    return;
+                }
+
+            }
+        });
+        //Registration
+        mSignUpHere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), RegistrationActivity.class));
+            }
+        });
+        //Password reset
+        mForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), ResetActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/expensemanager/RegistrationActivity.java
+++ app/src/main/java/com/example/expensemanager/RegistrationActivity.java
@@ -1,14 +1,79 @@
 package com.example.expensemanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
+    private EditText mEmail, mPass;
+    private Button btnReg;
+    private TextView mLogin;
+    private ProgressDialog mDialog;
+    //Firebase
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        mAuth = FirebaseAuth.getInstance();
+        mDialog = new ProgressDialog(this);
+        registration();
+    }
+
+    private void registration() {
+        mEmail = findViewById(R.id.Email_signup);
+        mPass = findViewById(R.id.Password_signup);
+        btnReg = findViewById(R.id.btn_signup);
+        mLogin = findViewById(R.id.login_here);
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = mEmail.getText().toString().trim();
+                String pass = mPass.getText().toString();
+                if (TextUtils.isEmpty(email)) {
+                    mEmail.setError("E-mail field is empty");
+                    return;
+                }
+                if (TextUtils.isEmpty(pass)) {
+                    mPass.setError("Password field is empty");
+                }
+                mDialog.setMessage("Processing");
+                mDialog.show();
+                mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            mDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "Successful registration", Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                        } else {
+                            mDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "Registration failed", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
     }
 }
