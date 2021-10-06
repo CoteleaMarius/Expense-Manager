d83a9b1354490f91883c9ce7208496c9b3a03c7a
Cotelea Marius <marius.cotelea1003@gmail.com>
2021-10-06 21:42:56 +0300

    I added login functionality

--- app/src/main/java/com/example/expensemanager/MainActivity.java
+++ app/src/main/java/com/example/expensemanager/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.expensemanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -9,16 +11,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     private EditText mEmail, mPass;
     private Button btnLogin;
     private TextView mForgotPassword, mSignUpHere;
-
+    private ProgressDialog mDialog;
+    //Firebase
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        mDialog = new ProgressDialog(this);
         loginDetails();
     }
 
@@ -41,7 +53,21 @@ public class MainActivity extends AppCompatActivity {
                     mPass.setError("Password field is empty.");
                     return;
                 }
-
+                mDialog.setMessage("Processing");
+                mDialog.show();
+                mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            mDialog.dismiss();
+                            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                            Toast.makeText(getApplicationContext(), "Login succesful", Toast.LENGTH_LONG).show();
+                        }else{
+                            mDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "Login failed", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
             }
         });
         //Registration
