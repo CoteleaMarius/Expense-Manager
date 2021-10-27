6f72ecc6c1005c7d8736dfa719976c88969d735d
Cotelea Marius <marius.cotelea1003@gmail.com>
2021-10-27 17:58:09 +0300

    I added the functionality to change fragments when navigation bar buttons are clicked

--- app/src/main/java/com/example/expensemanager/MainActivity.java
+++ app/src/main/java/com/example/expensemanager/MainActivity.java
@@ -30,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
+        if(mAuth.getCurrentUser()!=null){
+            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+        }
         mDialog = new ProgressDialog(this);
         loginDetails();
     }
