d5f08436734b9a176a71aff608e0f5db7d917bdc
Cotelea Marius <marius.cotelea1003@gmail.com>
2021-11-25 22:59:33 +0200

    Expense and Income data can now be added into the database

--- app/src/main/java/com/example/expensemanager/DashboardFragment.java
+++ app/src/main/java/com/example/expensemanager/DashboardFragment.java
@@ -1,19 +1,33 @@
 package com.example.expensemanager;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.expensemanager.Model.Data;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class DashboardFragment extends Fragment {
+
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -49,12 +63,22 @@ public class DashboardFragment extends Fragment {
     private boolean isOpen = false;
     //Animation
     private Animation FadOpen, FadClose;
+    //Firebase
+    private FirebaseAuth mAuth;
+    private DatabaseReference mIncomeDatabase;
+    private DatabaseReference mExpenseDatabase;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View myview = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser mUser = mAuth.getCurrentUser();
+        String uid = mUser.getUid();
+        mIncomeDatabase = FirebaseDatabase.getInstance().getReference().child("IncomeData").child(uid);
+        mExpenseDatabase = FirebaseDatabase.getInstance().getReference().child("ExpenseDatabase").child(uid);
+
         //Connect floating button to layout
         fab_main = myview.findViewById(R.id.fb_main_plus_btn);
         fab_income_btn = myview.findViewById(R.id.income_ft_btn);
@@ -68,6 +92,7 @@ public class DashboardFragment extends Fragment {
         fab_main.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                addData();
                 if (isOpen) {
                     fab_income_btn.startAnimation(FadClose);
                     fab_expense_btn.startAnimation(FadClose);
@@ -92,5 +117,146 @@ public class DashboardFragment extends Fragment {
             }
         });
         return myview;
+    }
+
+    //Floating button animation
+    private void ftAnimation() {
+        if (isOpen) {
+            fab_income_btn.startAnimation(FadClose);
+            fab_expense_btn.startAnimation(FadClose);
+            fab_income_btn.setClickable(false);
+            fab_expense_btn.setClickable(false);
+            fab_income_txt.startAnimation(FadClose);
+            fab_expense_txt.startAnimation(FadClose);
+            fab_income_txt.setClickable(false);
+            fab_expense_txt.setClickable(false);
+            isOpen = false;
+        } else {
+            fab_income_btn.startAnimation(FadOpen);
+            fab_expense_btn.startAnimation(FadOpen);
+            fab_income_btn.setClickable(true);
+            fab_expense_btn.setClickable(true);
+            fab_income_txt.startAnimation(FadOpen);
+            fab_expense_txt.startAnimation(FadOpen);
+            fab_income_txt.setClickable(true);
+            fab_expense_txt.setClickable(true);
+            isOpen = true;
+        }
+    }
+
+    private void addData() {
+        //Fab Button income
+        fab_income_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                incomeDataInsert();
+            }
+        });
+        fab_expense_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                expenseDataInsert();
+            }
+        });
+    }
+
+    public void incomeDataInsert() {
+        AlertDialog.Builder myDialog = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+        View myView = inflater.inflate(R.layout.custom_layout_for_insert_data, null);
+        myDialog.setView(myView);
+        final AlertDialog dialog = myDialog.create();
+        dialog.setCancelable(false);
+        final EditText edtAmount = myView.findViewById(R.id.amount_edt);
+        final EditText edtType = myView.findViewById(R.id.type_edt);
+        final EditText edtNote = myView.findViewById(R.id.note_edt);
+        Button btnSave = myView.findViewById(R.id.btnSave);
+        Button btnCancel = myView.findViewById(R.id.btnCancel);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String type = edtType.getText().toString().trim();
+                String amount = edtAmount.getText().toString().trim();
+                String note = edtNote.getText().toString().trim();
+                if (TextUtils.isEmpty(type)) {
+                    edtType.setError("Required field");
+                    return;
+                }
+                if (TextUtils.isEmpty(amount)) {
+                    edtAmount.setError("Required field");
+                    return;
+                }
+                int ourammountint = Integer.parseInt(amount);
+
+                if (TextUtils.isEmpty(note)) {
+                    edtNote.setError("Required field");
+                    return;
+                }
+                String id = mIncomeDatabase.push().getKey();
+                String mDate = DateFormat.getDateInstance().format(new Date());
+                Data data = new Data(ourammountint, type, note, id, mDate);
+                mIncomeDatabase.child(id).setValue(data);
+                Toast.makeText(getActivity(), "Data Added", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ftAnimation();
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+    public void expenseDataInsert() {
+        AlertDialog.Builder myDialog = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+        View myView = inflater.inflate(R.layout.custom_layout_for_insert_data, null);
+        myDialog.setView(myView);
+        final AlertDialog dialog = myDialog.create();
+        dialog.setCancelable(false);
+        final EditText amount = myView.findViewById(R.id.amount_edt);
+        final EditText type = myView.findViewById(R.id.type_edt);
+        final EditText note = myView.findViewById(R.id.note_edt);
+        Button btnSave = myView.findViewById(R.id.btnSave);
+        Button btnCancel = myView.findViewById(R.id.btnCancel);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tmAmount = amount.getText().toString().trim();
+                String tmType = type.getText().toString().trim();
+                String tmNote = note.getText().toString().trim();
+                if (TextUtils.isEmpty(tmAmount)) {
+                    amount.setError("Required field");
+                    return;
+                }
+                int inamount = Integer.parseInt(tmAmount);
+                if (TextUtils.isEmpty(tmType)) {
+                    type.setError("Required field");
+                    return;
+                }
+                if (TextUtils.isEmpty(tmNote)) {
+                    note.setError("Required field");
+                    return;
+                }
+                String id = mExpenseDatabase.push().getKey();
+                String mDate = DateFormat.getDateInstance().format(new Date());
+                Data data = new Data(inamount, tmType, tmNote, id, mDate);
+                mExpenseDatabase.child(id).setValue(data);
+                Toast.makeText(getActivity(), "Data added", Toast.LENGTH_SHORT).show();
+                ftAnimation();
+                dialog.dismiss();
+            }
+        });
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ftAnimation();
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 }
