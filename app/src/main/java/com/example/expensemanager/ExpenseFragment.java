33b5cb9a2852abf7967569ab181f3943fc694f7f
Cotelea Marius <marius.cotelea1003@gmail.com>
2021-12-06 22:20:22 +0200

    I created the update window

--- app/src/main/java/com/example/expensemanager/ExpenseFragment.java
+++ app/src/main/java/com/example/expensemanager/ExpenseFragment.java
@@ -2,25 +2,38 @@ package com.example.expensemanager;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ExpenseFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.expensemanager.Model.Data;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class ExpenseFragment extends Fragment {
+    //Firebase
+    private FirebaseAuth mAuth;
+    private DatabaseReference mExpenseDatabase;
+    //Recycleview
+    private RecyclerView recyclerView;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private TextView expenseSumResult;
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -28,15 +41,6 @@ public class ExpenseFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ExpenseFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ExpenseFragment newInstance(String param1, String param2) {
         ExpenseFragment fragment = new ExpenseFragment();
         Bundle args = new Bundle();
@@ -59,6 +63,89 @@ public class ExpenseFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_expense, container, false);
+        View myView = inflater.inflate(R.layout.fragment_expense, container, false);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser mUser = mAuth.getCurrentUser();
+        String uid = mUser.getUid();
+        mExpenseDatabase = FirebaseDatabase.getInstance().getReference().child("ExpenseDatabase").child(uid);
+        expenseSumResult = myView.findViewById(R.id.expense_txt_result);
+        recyclerView = myView.findViewById(R.id.recycler_id_expense);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        layoutManager.setStackFromEnd(true);
+        layoutManager.setReverseLayout(true);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+        mExpenseDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                int expenseSum = 0;
+
+                for (DataSnapshot mySnapshot : snapshot.getChildren()) {
+                    Data data = mySnapshot.getValue(Data.class);
+                    expenseSum += data.getAmount();
+                    String strExpenseSum = String.valueOf(expenseSum);
+                    expenseSumResult.setText(strExpenseSum);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        return myView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseRecyclerAdapter<Data, MyViewHolder> adapter = new FirebaseRecyclerAdapter<Data, MyViewHolder>
+                (
+                        Data.class,
+                        R.layout.expense_recycler_data,
+                        MyViewHolder.class,
+                        mExpenseDatabase
+                ) {
+            @Override
+            protected void populateViewHolder(MyViewHolder myViewHolder, Data model, int i) {
+                myViewHolder.setDate(model.getDate());
+                myViewHolder.setType(model.getType());
+                myViewHolder.setNote(model.getNote());
+                myViewHolder.setAmount(model.getAmount());
+            }
+        };
+        recyclerView.setAdapter(adapter);
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        View mView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        private void setDate(String date) {
+            TextView mDate = mView.findViewById(R.id.date_txt_expense);
+            mDate.setText(date);
+        }
+
+        private void setType(String type) {
+            TextView mType = mView.findViewById(R.id.type_txt_expense);
+            mType.setText(type);
+        }
+
+        private void setNote(String note) {
+            TextView mNote = mView.findViewById(R.id.Note_txt_expense);
+            mNote.setText(note);
+        }
+
+        private void setAmount(int amount) {
+            TextView mAmount = mView.findViewById(R.id.ammount_txt_expense);
+            String strAmount = String.valueOf(amount);
+            mAmount.setText(strAmount);
+        }
+
     }
 }
